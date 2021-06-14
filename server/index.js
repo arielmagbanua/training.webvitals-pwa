01e3a2d8d80aec0753f356ed049089d54cfd9589
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-06-14 18:52:30 +0800

    Server index file.

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// routes
+const publicRoutes = require('./routes/public');
+
+const serverApp = express();
+
+// parse request body
+serverApp.use(bodyParser.urlencoded({extended: false}));
+
+// set a static directory
+serverApp.use(express.static(path.join(__dirname, 'public')));
+
+serverApp.use(publicRoutes);
+
+const server = serverApp.listen(process.env.port || 8080, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(`App listening at https://${host}:${port}`);
+});
