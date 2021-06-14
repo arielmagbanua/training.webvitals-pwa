d2d9ea3648e13df836fc44f9f20d35f5d106fc96
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-06-14 22:13:19 +0800

    updated the tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,32 @@
-const gulp = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 
 const paths = {
   views: {
     src: 'src/views/*.html',
     dest: 'dist/views/'
+  },
+  css: {
+    src: 'src/css/*',
+    dest: 'dist/public/css/'
   }
 };
 
-function defaultTask(cb) {
-  // place code for your default task here
+const views = (cb) => {
+  src(paths.views.src)
+    .pipe(dest(paths.views.dest));
+
   cb();
-
-  console.log('default task.')
 }
 
-exports.default = defaultTask
+const styles = (cb) => {
+  src(paths.css.src)
+    .pipe(dest(paths.css.dest));
 
-function views() {
-  return gulp.src(paths.views.src)
-    .pipe(gulp.dest(paths.views.dest))
+  cb();
 }
 
-exports.views = views;
+exports.watch = () => {
+  watch(['src/css/*', 'src/views/*.html'], series(styles, views));
+}
+
+exports.default = series(styles, views);
