13cd23b1caa8f9d0869734d6ff512d5c197a0ed4
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-06-15 18:00:33 +0800

    Added javascript processing.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const { src, dest, series, watch } = require('gulp');
 const sass = require('gulp-sass');
+const sourcemaps = require("gulp-sourcemaps");
+const babel = require("gulp-babel");
+const rollup = require('gulp-rollup');
+const concat = require('gulp-concat');
 
 const paths = {
   views: {
@@ -9,6 +13,19 @@ const paths = {
   scss: {
     src: 'src/css/*.scss',
     dest: 'dist/public/css/'
+  },
+  js: {
+    src: 'src/js/*.js',
+    dest: 'dist/public/js/',
+    concat: {
+      files: [
+        './node_modules/bootstrap/dist/js/bootstrap.js'
+      ]
+    },
+    files: {
+      index: 'index.js',
+      products: 'products.js'
+    }
   }
 };
 
@@ -21,14 +38,30 @@ const views = (cb) => {
 
 const styles = (cb) => {
   src(paths.scss.src)
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write("."))
     .pipe(dest(paths.scss.dest));
 
   cb();
 }
 
-exports.watch = () => {
-  watch(['src/css/*', 'src/views/*.html'], {ignoreInitial: false}, series(styles, views));
+const scripts = (cb) => {
+  src([...paths.js.concat.files, paths.js.src])
+    .pipe(sourcemaps.init())
+    .pipe(concat(paths.js.files.index))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(paths.js.dest));
+
+  cb();
 }
 
-exports.default = series(styles, views);
+exports.watch = () => {
+  watch(
+    ['src/css/*.css', paths.scss.src, paths.js.src, paths.views.src],
+    {ignoreInitial: false},
+    series(styles, scripts, views)
+  );
+}
+
+exports.default = series(styles, scripts, views);
