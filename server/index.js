d297d1b2cb8d56cbd290545754e3fea542bca31a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-06-28 21:30:21 +0800

    Configured handlebars.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const handlebars  = require('express-handlebars');
+
 const path = require('path');
 
 // for json server
 const jsonServer = require('json-server');
 const apiRouter = jsonServer.router(path.join(__dirname, 'database/db.json'));
 
+const serverApp = express();
+
+// sets the engine and configuration.
+serverApp.set('view engine', 'hbs');
+
+const hbs = handlebars.create({
+  layoutsDir: path.join(__dirname, '../src/views/layouts'),
+  // partialsDir: path.join(__dirname, '../src/views/partials'),
+  // defaultLayout: 'index',
+  extname: 'hbs'
+});
+serverApp.engine('hbs', hbs.engine);
+
+serverApp.set('views', path.join(__dirname, '../src/views'));
+
 // routes
 const publicRoutes = require('./routes/public');
-
-const serverApp = express();
 
 // parse request body
 serverApp.use(bodyParser.urlencoded({extended: false}));
