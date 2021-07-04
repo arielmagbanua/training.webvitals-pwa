1c85ed76a0d6f299d4d677088658609f97c03543
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-07-04 20:40:14 +0800

    Code clean up.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const handlebars  = require('express-handlebars');
+const handlebars = require('express-handlebars');
 
 const path = require('path');
 
