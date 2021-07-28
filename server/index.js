ae9119b0bfc578e10799dc2b5f8c5ad3dfef3f24
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-07-28 23:09:06 +0800

    Fix the use of port.

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ serverApp.use(publicRoutes);
 // json server routes
 serverApp.use('/api', apiRouter);
 
-const server = serverApp.listen(process.env.port || 8080, () => {
+const server = serverApp.listen(process.env.PORT || 8080, () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log(`App listening at https://${host}:${port}`);
