d417d42a62f1c0cd004ee271f226ca56caf263b9
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-08-04 19:57:46 +0800

    Fixed the reponsive grid.

--- src/js/products.js
+++ src/js/products.js
@@ -6,7 +6,10 @@ const allProducts = async () => {
 const createCardElement = (product) => {
   const cardElement = document.createElement('div');
   cardElement.classList.add('card-container');
-  cardElement.classList.add('col-3');
+  cardElement.classList.add('col-xl-3');
+  cardElement.classList.add('col-lg-4');
+  cardElement.classList.add('col-md-6');
+  cardElement.classList.add('col-sm-12');
 
   cardElement.innerHTML = `
     <div class="card product-list-card hover-elevated m-1 p-0 col-12">
