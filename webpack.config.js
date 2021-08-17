da90aa5f338a57d66aced1b70ef562d434a445d3
unknown <magbanua.c.ariel@gmail.com>
2021-08-18 00:03:04 +0800

    Added babe loader.

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,20 @@ module.exports = {
       './node_modules/bootstrap/dist/js/bootstrap.js'
     ]
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist/public/js'),
