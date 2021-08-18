390f6d78563b568a601a4ef08ddd9637ac949769
unknown <magbanua.c.ariel@gmail.com>
2021-08-18 21:48:05 +0800

    Added resolving of hashed js assets.

--- server/routes/public.js
+++ server/routes/public.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const path = require('path');
+const findFiles = require('file-regex');
 const axios = require('axios').default;
 require('dotenv').config();
-
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+const rootDir = require('../utils/path');
+const publicJsDir = path.join(rootDir, '../dist/public/js');
+
+async function findAsset(directory, regexPattern) {
+  return await findFiles(directory, regexPattern)
+    .then((result) => {
+      return result[0].file;
+    })
+    .catch((_) => {
+      return 'index.bundle.js';
+    });
+}
+
+router.get('/', async (req, res, next) => {
+  // resolve the correct index js bundle
+  const pageJs = await findAsset(publicJsDir, /index\.([a-zA-Z0-9]+)\.bundle\.js$/);
+  const vendorJs = await findAsset(publicJsDir, /vendor\.([a-zA-Z0-9]+)\.bundle\.js$/);
+
   res.render('index', {
-    layout: 'index'
+    layout: 'index',
+    jsPath: '/js/' + pageJs,
+    vendorJsPath: '/js/' + vendorJs,
   });
 });
 
@@ -14,8 +34,11 @@ router.get('/products', async (req, res, next) => {
   const sku = req.query.sku;
 
   if (!sku) {
+    // resolve the correct products js bundle
+    const pageJs = await findAsset(publicJsDir, /products\.([a-zA-Z0-9]+)\.bundle\.js$/);
     return res.render('products', {
-      layout: 'products'
+      layout: 'products',
+      jsPath: '/js/' + pageJs
     });
   }
 
@@ -26,9 +49,13 @@ router.get('/products', async (req, res, next) => {
   const productUrl = appUrl + '/api/products/' + sku;
   const product = await axios.get(productUrl);
 
+  // resolve the correct product js bundle
+  const pageJs = await findAsset(publicJsDir, /product\.([a-zA-Z0-9]+)\.bundle\.js$/);
+
   res.render('product', {
     layout: 'product',
-    product: product.data
+    product: product.data,
+    jsPath: '/js/' + pageJs
   });
 });
 
