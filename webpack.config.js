a47948b866ec3beca7cf0e8bb9164de8809007d2
unknown <magbanua.c.ariel@gmail.com>
2021-08-18 19:29:46 +0800

    Implemented modes.

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,49 @@
 const path = require('path');
 
-module.exports = {
-  entry: {
-    index: [
-      './src/js/index.styles.js',
-      './src/js/index.js'
-    ],
-    products: [
-      './src/js/products.styles.js',
-      './src/js/products.js'
-    ],
-    vendor: [
-      './node_modules/bootstrap/dist/js/bootstrap.js'
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+module.exports = (env, argv) => {
+  return {
+    entry: {
+      index: [
+        './src/js/index.styles.js',
+        './src/js/index.js'
+      ],
+      products: [
+        './src/js/products.styles.js',
+        './src/js/products.js'
+      ],
+      vendor: [
+        './node_modules/bootstrap/dist/js/bootstrap.js'
+      ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
+        },
+        {
+          test: /\.(scss|sass|css)$/,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
         }
-      },
-      {
-        test: /\.(scss|sass|css)$/,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      }
-    ]
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist/public/js'),
-  },
-};
+      ]
+    },
+    output: {
+      filename: argv.mode == 'development' ? '[name].bundle.js' : '[name].[contenthash].bundle.js',
+      path: path.resolve(__dirname, 'dist/public/js'),
+      clean: true,
+    },
+  };
+}
